3f1a55e77b8a739fd48fb3944cb6e0fda73f6cb4
Fabio Nobre <f.oliveiranobre@gmail.com>
2018-07-13 23:16:56 -0300

    Insercao de usuario com POST

--- src/main/java/com/fabionobre/workshopmongo/services/UserService.java
+++ src/main/java/com/fabionobre/workshopmongo/services/UserService.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.fabionobre.workshopmongo.domain.User;
+import com.fabionobre.workshopmongo.dto.UserDTO;
 import com.fabionobre.workshopmongo.repository.UserRepository;
 import com.fabionobre.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -20,10 +22,17 @@ public class UserService {
 	}
 	
 	public User findById(String id) {
-
 		Optional<User> obj = repo.findById(id);
-
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail()); 
+	}
+	
+	
 }
