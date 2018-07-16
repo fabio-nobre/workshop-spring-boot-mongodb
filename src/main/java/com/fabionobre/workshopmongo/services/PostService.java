df664410fe000ea08c97c4274c8822889a8d2077
Fabio Nobre <f.oliveiranobre@gmail.com>
2018-07-16 06:19:01 -0300

    Obtendo um post por id

---
+++ src/main/java/com/fabionobre/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.fabionobre.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fabionobre.workshopmongo.domain.Post;
+import com.fabionobre.workshopmongo.repository.PostRepository;
+import com.fabionobre.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
